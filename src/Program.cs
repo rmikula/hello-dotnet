3b6c99c2af7678428e8864ae339e336788670c96
Roman Mikula <rmikula@gmail.com>
2018-10-26 15:07:28 -0700

    Init

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dotnet 2!");
+        }
+    }
+}
